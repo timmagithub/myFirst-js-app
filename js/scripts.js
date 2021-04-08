4af5f1d3af1dc6233d69224e2f8e86072127e2e8
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-04-07 22:44:17 -0400

    Update scripts.js
    Changed js from intro testing codes, to prep for exercise project. Added array(pokemonList) of three objects(pokemon), defined by three keys(name, height, type).

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert('Hello World');
-
-let favoriteFood ='Chee Pang House SpecialLo Mein';
-
-document.write(favoriteFood)
+let pokemonList [
+    { name: 'charmander', height: .6, type: 'fire'};
+    { name: 'krabby', height: .4, type: 'water'};
+    { name: 'diglett', height: .2, type: 'ground'}
+];
