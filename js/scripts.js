007e5378f18f61bd15a43e45da11aa8c8fea4e87
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-04-08 21:13:45 -0400

    Update script.js
    Corrected missing item from Array code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let pokemonList [
     { name: 'charmander', height: .6, type: 'fire'};
     { name: 'krabby', height: .4, type: 'water'};
     { name: 'diglett', height: .2, type: 'ground'}
+let pokemonList = [
 ];
