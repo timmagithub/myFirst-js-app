92569d3670c27e759d1a23a881101bb010ccd9de
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-04-14 22:33:48 -0400

    Update scripts.js
    Added loop to write the names and heights of pokemon on index page. Also adds a conditional message based on pokemon height.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ let pokemonList = [
     { name: 'Krabby', height: 0.4, types: ['water', 'none']},
     { name: 'Venomoth', height: 1.5, types: ['bug', 'poison']}
 ];
+
+/*the loop below will operate in the browser, writing the names and 
+heights of the pokemon until the array has run out.*/
+for (let i=0; i<pokemonList.length; i++) {
+    if (pokemonList[i].height>1) {
+    document.write(pokemonList[i].name + ' (height: ' + 
+    pokemonList[i].height + '). Wow! That\'s a big one! ')
+    }
+    else document.write(pokemonList[i].name + ' (height: ' + 
+    pokemonList[i].height + '). ')
+}
