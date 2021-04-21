a5b22b118397de5119c5cb72124daabf009d80b8
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-04-20 22:39:43 -0400

    Update scripts.js
    Added for clarification when reading the code, moving on from the first version of the app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+/* Ver 1.1
+first rendition of code. didn't want to disregard.
+
 let pokemonList = [
     { name: 'Charmander', height: 0.6, types: ['fire', 'none']},
     { name: 'Krabby', height: 0.4, types: ['water', 'none']},
     { name: 'Venomoth', height: 1.5, types: ['bug', 'poison']}
 ];
 
-/*the loop below will operate in the browser, writing the names and 
-heights of the pokemon until the array has run out.*/
+/the loop below will operate in the browser, writing the names and 
+heights of the pokemon until the array has run out./
 for (let i=0; i<pokemonList.length; i++) {
     if (pokemonList[i].height>1) {
     document.write(pokemonList[i].name + ' (height: ' + 
@@ -13,4 +16,3 @@ for (let i=0; i<pokemonList.length; i++) {
     }
     else document.write(pokemonList[i].name + ' (height: ' + 
     pokemonList[i].height + '). ')
-}
