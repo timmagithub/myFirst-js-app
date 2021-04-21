9476225a0c37f7dcdc0bfc168f3e220f39a0a144
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-04-20 22:41:23 -0400

    Update scripts.js
    Array was added to a function inside of IIFE, and assigned to a variable to take away global-ness.

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,12 @@ for (let i=0; i<pokemonList.length; i++) {
 
 /* Ver1.2 */
 
+    /* list inside function wrapped in IIFE. this cements the list as 
+    local variables, accessed and modified through extreme, deliberate, 
+    means. */
+let pokemonRepository = (function() {
+    let pokemonList = [
+      { name: 'Charmander', height: 0.6, types: ['fire', 'none']},
+      { name: 'Krabby', height: 0.4, types: ['water', 'none']},
+      { name: 'Venomoth', height: 1.5, types: ['bug', 'poison']}
+    ]
